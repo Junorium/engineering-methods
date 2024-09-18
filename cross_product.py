52ca438e365b34f0668627ddabe06fff99a08c2b
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-09-17 22:02:01 -0400

    Update cross_product.py

--- cross_product.py
+++ cross_product.py
@@ -1,16 +1,19 @@
 # vector input as a list of lists
 # vectors = [[vector1], [vector2]]
 
-def cross_product_xy(vectors):
+def vector_cross_product_xy(vectors):
   vector_1 = vectors[0]
   vector_2 = vectors[1]
 
   return [vector_1[0] * vector_2[1], vector_1[1] * vector_2[0]]
 
-def cross_product_xyz(vectors):
+def vector_cross_product_xyz(vectors):
   vector_1 = vectors[0]
   vector_2 = vectors[1]
 
   return [(vector_1[1]*vector_2[2]) - (vector_1[2]*vector_1[1]),
           (vector_1[0]*vector_2[2]) - (vector_1[2]*vector_2[0]), 
           (vector_1[0]*vector_2[1]) - (vector_1[1]*vector_2[0])]
+
+def cross_product(matrix): # input must be square matrix
+  
