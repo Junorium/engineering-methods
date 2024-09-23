75efd44f1a250ff620697cc2691d7da8a36c4d01
Junorium <johnrichard.perez.2004@gmail.com>
2024-09-23 11:44:55 -0400

    init new vector calc

--- vector_calculations.py
+++ vector_calculations.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sympy import symbols, diff
+
 # vector parameters: should be three dimensions, parametrized
 # vector calculations possible:
 # curvature k
@@ -6,15 +9,30 @@
 # projection calculations
 # cross product, dot product
 
-vector1 = input()
-vector2 = input() # should be the same dimensions
+vector_list = input() #list of lists
 dimensions = input("enter dimensions")
 
-def dot_product(vector1, vector2, dim=3):
-    return None
+def dot_product(vector_list, dim=3):
+    product = 0
+    # component matching
+    # element wise multiplication
+    # element wise aggregation
+    dim_comp = [1] * dim # default identity
+    for vector in vector_list:
+        for index,elem in enumerate(vector):
+            dim_comp[index] = dim_comp[index] * elem
+    product = sum(dim_comp)
+
+    return product
 
 def cross_product(vector1, vector2, dim=3):
     if dim != [3,7]:
         print("Dimensions do not match, not 3 or 7.")
         return None
 
+
+def curvature():
+    return None
+
+def projection(vector, base_vector):
+    return None
