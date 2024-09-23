f0db69f1e282942446f21445a61581eb43bfdac0
Junorium <johnrichard.perez.2004@gmail.com>
2024-09-23 01:15:03 -0400

    added functions

--- vector_calculations.py
+++ vector_calculations.py
@@ -1,2 +1,20 @@
-vector1 = [x,y,z]
-vector2 = [a,b,c]
+# vector parameters: should be three dimensions, parametrized
+# vector calculations possible:
+# curvature k
+# unit tangent T and unit normal N
+# osculating curve calculation
+# projection calculations
+# cross product, dot product
+
+vector1 = input()
+vector2 = input() # should be the same dimensions
+dimensions = input("enter dimensions")
+
+def dot_product(vector1, vector2, dim=3):
+    return None
+
+def cross_product(vector1, vector2, dim=3):
+    if dim != [3,7]:
+        print("Dimensions do not match, not 3 or 7.")
+        return None
+
