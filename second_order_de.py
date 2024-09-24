0b4793b4c0f9f8a8b27092aac12df68ef9e0f8d5
Junorium <johnrichard.perez.2004@gmail.com>
2024-09-24 11:39:06 -0400

    init second_order_de.py w objectives

---
+++ second_order_de.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# SODE should be in form x'' + Ax' + Bx = 0
+# assume system containing c, m, g, k
+# c is damping factor
+# k is stiffness factor
+
+def SODE():
+    return None
